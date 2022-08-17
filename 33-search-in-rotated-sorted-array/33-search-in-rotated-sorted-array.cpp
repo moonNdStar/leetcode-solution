b9e73e7e25e1df82cc36ca013989d7da6dbb562c
moonNdStar <77716940+moonNdStar@users.noreply.github.com>
2022-08-18 01:10:33 +0530

    Time: 5 ms (62.33%), Space: 11 MB (75.25%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        if(n==0)
+        {
+                return -1;
+        }
+       
+        for(int i=0;i<n;i++){
+            if(target==nums[i]){
+                return i;
+            }
+         }
+    return -1;
+    }
+};
